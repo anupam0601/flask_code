2565faec9dd2aee08c846635b4ca75b80f87d0ad
anupam0601 <adebnath1989@gmail.com>
2016-02-22 14:17:25 +0530

    mongodb extended

--- app.py
+++ app.py
@@ -2,9 +2,18 @@
 from flask import Flask, render_template, redirect, \
     url_for, request, session, flash
 from functools import wraps
+from flask import request
+from pymongo import MongoClient
+import os
+
 
 # create the application object
 app = Flask(__name__)
+
+# Providing the details for mongoclient and database
+client = MongoClient('localhost',27017)
+db = client.anupam
+
 
 # config
 app.secret_key = 'my precious'
@@ -35,6 +44,15 @@ def welcome():
     return render_template('welcome.html')  # render a template
 
 
+#mongodb function to fetch data
+@app.route('/todo')
+@login_required
+def todo():
+   _items = db.movie.find()
+   items = [item for item in _items]  
+   return render_template('todo.html', items=items)
+
+
 # route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -45,24 +63,28 @@ def login():
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
-            flash('You were logged in.')
+            #flash('You were logged in.')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
 # for highcharts
+# Put the  data in xAxis category and you can change the color with the "color" option set to the color code
 @app.route('/graph')
 @login_required
-def index(chartID = 'chart_ID', chart_type = 'bar', chart_height = 800):
+def index(chartID = 'chart_ID', chart_type = 'area', chart_height = 900):
     chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,"zoomType" : 'x'}
-    series = [{"name": 'CPU', "data": [6.3,8.8,6.9,6,7,8,8,9]}, {"name": 'MEM', "data": [7.8,10.5,9.36,7,8,8,9,10]}]
-    title = {"text": 'Performance Stats'}
-    xAxis = {"categories": ['2015-07-29 16:25:03','2015-07-29 16:25:10','2015-07-29 16:25:10','2015-07-29 16:25:10','2015-07-29 16:25:10','2015-07-29 16:25:10','2015-07-29 16:25:10','2015-07-29 16:25:10']}
-    yAxis = {"title": {"text": 'CPU and MEM'}}
+    series = [{"name": "DEFECTS FOUND", "data": [0,12,0,26,11,22,13,5,13,12,13,7,4,4,2,8,12,2,2,5,3,15],"color" : '#ff0004'}] #{"name": 'QA CYCLE', "data": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]}]
+    title = {"text": 'Bugs Overview'}
+    xAxis = {"name":'Cycle',"categories": ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']}
+    #yAxis = 
+    
+    yAxis = {"title": {"text": 'Bugs'}}
+    xAxis = {"title": {"text":"Cycle number [cycle #2 was abandoned]"}}
     return render_template('graph.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
     #return render_template('graph.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
 
-
+# Fetch the data from mongodb:
 
 @app.route('/logout')
 @login_required
@@ -71,7 +93,15 @@ def logout():
     flash('You were logged out.')
     return redirect(url_for('welcome'))
 
+@app.route('/testResults')
+@login_required
+def test_results():
+    return render_template('testResults.html')  # render a template
+
+
+
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True,host ='0.0.0.0')
+    app.run(debug=True,host = '0.0.0.0')
